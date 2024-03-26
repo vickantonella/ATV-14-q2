2b12be5ccf3ec000a8f44721b6dcd7aeec3647d0
Victoria Antonella <134626319+vickantonella@users.noreply.github.com>
2024-03-26 14:52:25 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-// db.js
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'alanbcdd',
-  password: 'exatamente',
-  database: 'pweb2'
+  host: process.env.host,
+  user: process.env.user,
+  port: process.env.port,
+  password: process.env.password,
+  database: process.env.database
 });
 
 connection.connect((err) => {
